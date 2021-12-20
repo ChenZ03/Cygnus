937eea12674be4896ee0697f1123a15f372faa0b
Chen Zhung <46520696+Xectroz@users.noreply.github.com>
2021-12-20 13:20:26 +0800

    added component template

---
+++ src/components/Login.js
@@ -0,0 +1,11 @@
+import React, {useState} from 'react'
+import {Button} from 'react-bootstrap'
+import Background from '../assets/bg3.jpg'
+
+function Login() {
+    return(
+        <h1>Login</h1>
+    )
+}
+        
+export default Login
